f4cd818324eae5d99e41f6e4f1c570e79572babc
neroist <pmpiga1@gmail.com>
2024-06-03 00:16:32 -0400

    add scripts to call script that generate other scripts

---
+++ gen_all_scripts.bat
@@ -0,0 +1,20 @@
+@echo off
+
+doskey gen_script = python gen_script.py
+
+:: espanso
+gen_script espanso -f "./sitelen-pona-espanso.yml"
+
+:: macos input plugin
+gen_script macos -f"./sitelen-pona.inputplugin"
+
+:: ahk scripts
+IF NOT EXIST "./ahk-scripts/" mkdir "./ahk-scripts/"
+gen_script ahk -f "./ahk-scripts/sitelen-pona.ahk"
+gen_script ahk -tf "./ahk-scripts/sitelen-pona-toggle.ahk"
+gen_script ahk -sf "./ahk-scripts/stl-pon.ahk"
+
+:: ibus tables
+IF NOT EXIST "./ibus-tables/" mkdir "./ibus-tables/"
+gen_script ibus -f "./ibus-tables/sitelen-pona.ibus-table"
+gen_script ibus -sf "./ibus-tables/stl-pon.ibus-table"
---
+++ gen_all_scripts.sh
@@ -0,0 +1,18 @@
+alias gen_script="python gen_script.py"
+
+# espanso
+gen_script espanso -f "./sitelen-pona-espanso.yml"
+
+# macos input plugin
+gen_script macos -f "./sitelen-pona.inputplugin"
+
+# ahk scripts
+mkdir -p "./ahk-scripts/"
+gen_script ahk    -f "./ahk-scripts/sitelen-pona.ahk"
+gen_script ahk -t -f "./ahk-scripts/sitelen-pona-toggle.ahk"
+gen_script ahk -s -f "./ahk-scripts/stl-pon.ahk"
+
+# ibus tables
+mkdir -p "./ibus-tables/"
+gen_script ibus    -f "./ibus-tables/sitelen-pona.ibus-table"
+gen_script ibus -s -f "./ibus-tables/stl-pon.ibus-table"
