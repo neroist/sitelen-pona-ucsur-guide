a270d604a71ec63df82e7215afb2e3650c1984dd
neroist <pmpiga1@gmail.com>
2024-06-03 01:28:14 -0400

    seperate flags & options

--- gen_all_scripts.bat
+++ gen_all_scripts.bat
@@ -10,11 +10,11 @@ gen_script macos -f "./sitelen-pona.inputplugin"
 
 :: ahk scripts
 IF NOT EXIST "./ahk-scripts/" mkdir "./ahk-scripts/"
-gen_script ahk -f "./ahk-scripts/sitelen-pona.ahk"
-gen_script ahk -tf "./ahk-scripts/sitelen-pona-toggle.ahk"
-gen_script ahk -sf "./ahk-scripts/stl-pon.ahk"
+gen_script ahk    -f "./ahk-scripts/sitelen-pona.ahk"
+gen_script ahk -t -f "./ahk-scripts/sitelen-pona-toggle.ahk"
+gen_script ahk -s -f "./ahk-scripts/stl-pon.ahk"
 
 :: ibus tables
 IF NOT EXIST "./ibus-tables/" mkdir "./ibus-tables/"
-gen_script ibus -f "./ibus-tables/sitelen-pona.ibus-table"
-gen_script ibus -sf "./ibus-tables/stl-pon.ibus-table"
+gen_script ibus    -f "./ibus-tables/sitelen-pona.ibus-table"
+gen_script ibus -s -f "./ibus-tables/stl-pon.ibus-table"
