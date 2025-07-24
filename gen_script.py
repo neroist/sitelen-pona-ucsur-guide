9784ca7b175299680f5a7bbe3719e982ccdeed82
niko <pmpiga1@gmail.com>
2025-07-24 14:01:10 -0400

    indent multiline strings (so code collapsing in editors works correctly)

--- gen_script.py
+++ gen_script.py
@@ -172,162 +172,163 @@ def ibus_table(words, short: bool = False) -> str:
 
     table_id = str(uuid.uuid4())
     now = datetime.datetime.now().strftime("%Y%m%d")
-    table = f"""### {gen_header}
+    table = dedent(f"""
+    ### {gen_header}
 
-### File header must not be modified
-### This file must be encoded into UTF-8.
-### This table under LGPL
-### comments start with ### not single #
-### Derive from the format of SCIM Table, so you can modify the table from
-### scim-tables' table
-SCIM_Generic_Table_Phrase_Library_TEXT
-VERSION_1_0
+    ### File header must not be modified
+    ### This file must be encoded into UTF-8.
+    ### This table under LGPL
+    ### comments start with ### not single #
+    ### Derive from the format of SCIM Table, so you can modify the table from
+    ### scim-tables' table
+    SCIM_Generic_Table_Phrase_Library_TEXT
+    VERSION_1_0
 
-### Begin Table definition.
-BEGIN_DEFINITION
+    ### Begin Table definition.
+    BEGIN_DEFINITION
 
-### License
-LICENSE = LGPL
+    ### License
+    LICENSE = LGPL
 
-### An unique id to distinguish this table among others.
-### Use uuidgen to generate this kind of id.
-UUID = {table_id}
+    ### An unique id to distinguish this table among others.
+    ### Use uuidgen to generate this kind of id.
+    UUID = {table_id}
 
-### A unique number indicates the version of this file.
-### For example the last modified date of this file.
-### This number must be less than 2^32.
-### Just make your table version-able
-SERIAL_NUMBER = {now}
+    ### A unique number indicates the version of this file.
+    ### For example the last modified date of this file.
+    ### This number must be less than 2^32.
+    ### Just make your table version-able
+    SERIAL_NUMBER = {now}
 
-### ICON can be any format as long as your pygtk can recognized
-### the most widely ones are "png" and "svg", letter one is recommended
-ICON = ibus-table.svg
+    ### ICON can be any format as long as your pygtk can recognized
+    ### the most widely ones are "png" and "svg", letter one is recommended
+    ICON = ibus-table.svg
 
-### The symbol to be displayed in IM switchers
-SYMBOL = 󱥬
+    ### The symbol to be displayed in IM switchers
+    SYMBOL = 󱥬
 
-### The default name of this table, this is needed
-NAME = sitelen pona 4.0
+    ### The default name of this table, this is needed
+    NAME = sitelen pona 4.0
 
-### The local names of this table, this is optional
-### NAME.zh_CN = 形码
-### NAME.zh_HK = 形碼
-### NAME.zh_TW = 形碼
+    ### The local names of this table, this is optional
+    ### NAME.zh_CN = 形码
+    ### NAME.zh_HK = 形碼
+    ### NAME.zh_TW = 形碼
 
-### Description
-DESCRIPTION = sitelen pona input method for IBus, ported from nasin-nanpa's ahk script.
+    ### Description
+    DESCRIPTION = sitelen pona input method for IBus, ported from nasin-nanpa's ahk script.
 
-### Supported languages of this table
-### sigle "zh_CN" just be recognized as zh_CN,
-### but "zh_CN, zh_HK" or more zh_XX will be recognized as zh;
-### and "en_US, zh_CN" will be just ignored.
-LANGUAGES = en_US
+    ### Supported languages of this table
+    ### sigle "zh_CN" just be recognized as zh_CN,
+    ### but "zh_CN, zh_HK" or more zh_XX will be recognized as zh;
+    ### and "en_US, zh_CN" will be just ignored.
+    LANGUAGES = en_US
 
-### The author of this table
-AUTHOR = jan Komi
+    ### The author of this table
+    AUTHOR = jan Komi
 
-### Prompt string to be displayed in the status area, CN will be replaced by
-### the gettext tools in runtime as 中.
-STATUS_PROMPT = toki
+    ### Prompt string to be displayed in the status area, CN will be replaced by
+    ### the gettext tools in runtime as 中.
+    STATUS_PROMPT = toki
 
-### Valid input chars.
-VALID_INPUT_CHARS = ()*+-.:<>[]_aeijklmnopstuwz{{}}~
+    ### Valid input chars.
+    VALID_INPUT_CHARS = ()*+-.:<>[]_aeijklmnopstuwz{{}}~
 
-### Layout
-LAYOUT = us
+    ### Layout
+    LAYOUT = us
 
-### The max number of input keys for every phrase or character.
-MAX_KEY_LENGTH = 20
+    ### The max number of input keys for every phrase or character.
+    MAX_KEY_LENGTH = 20
 
-### Use auto_commit mode as default
-AUTO_COMMIT = TRUE
+    ### Use auto_commit mode as default
+    AUTO_COMMIT = TRUE
 
-### Automatically selects the first phrase when typing
-AUTO_SELECT = FALSE
+    ### Automatically selects the first phrase when typing
+    AUTO_SELECT = FALSE
 
-### Use full width punctuation by default
-DEF_FULL_WIDTH_PUNCT = FALSE
-### Not use full width letter by default
-DEF_FULL_WIDTH_LETTER = FALSE
+    ### Use full width punctuation by default
+    DEF_FULL_WIDTH_PUNCT = FALSE
+    ### Not use full width letter by default
+    DEF_FULL_WIDTH_LETTER = FALSE
 
-### Whether user are allow to define phrase, default is true
-### You have to define the word construction rules below.
-### For input methods which do not input phrases, set this to False
-USER_CAN_DEFINE_PHRASE = FALSE
+    ### Whether user are allow to define phrase, default is true
+    ### You have to define the word construction rules below.
+    ### For input methods which do not input phrases, set this to False
+    USER_CAN_DEFINE_PHRASE = FALSE
 
-### Whether support PinYin Mode, default is true.
-### this feature is just for Chinese, set it to False if your IM is not
-### Chinese.
-PINYIN_MODE = FALSE
+    ### Whether support PinYin Mode, default is true.
+    ### this feature is just for Chinese, set it to False if your IM is not
+    ### Chinese.
+    PINYIN_MODE = FALSE
 
-### If true then the phrases' frequencies will be adjusted dynamically
-### according your using frequency.
-DYNAMIC_ADJUST = TRUE 
+    ### If true then the phrases' frequencies will be adjusted dynamically
+    ### according your using frequency.
+    DYNAMIC_ADJUST = TRUE 
 
-### Some characters whose frequencies should be fix all the time, e.g. 
-### some punctuations
-### NO_CHECK_CHARS = 
+    ### Some characters whose frequencies should be fix all the time, e.g. 
+    ### some punctuations
+    ### NO_CHECK_CHARS = 
 
-### Rules for constructing user defined phrase
-### "ce" stands for "ci equal", a Chinese English :), means "phrase length
-### equal to", thus ce2 -> phrase length equal to 2; and "ca" means "phrase
-### length equal or above", so ca4 -> phrase length equal or above 4.
-### p21 -> the 1st key of 2nd character in the phrase, and so on.
-### Each rule separate via ";". 
-### Example below is a complete rule-set, 
-### becuase [2,2] ∩ [3,3] ∩ [4,+∞] = [2,+∞], which is the range of length
-### of phrase. This have to be satisfied if you need ibus-table to build up
-### your own inputed phrase via your daily using.
-### RULES = ce2:p11+p12+p21+p22;ce3:p11+p21+p22+p31;ca4:p11+p21+p31+p41
+    ### Rules for constructing user defined phrase
+    ### "ce" stands for "ci equal", a Chinese English :), means "phrase length
+    ### equal to", thus ce2 -> phrase length equal to 2; and "ca" means "phrase
+    ### length equal or above", so ca4 -> phrase length equal or above 4.
+    ### p21 -> the 1st key of 2nd character in the phrase, and so on.
+    ### Each rule separate via ";". 
+    ### Example below is a complete rule-set, 
+    ### becuase [2,2] ∩ [3,3] ∩ [4,+∞] = [2,+∞], which is the range of length
+    ### of phrase. This have to be satisfied if you need ibus-table to build up
+    ### your own inputed phrase via your daily using.
+    ### RULES = ce2:p11+p12+p21+p22;ce3:p11+p21+p22+p31;ca4:p11+p21+p31+p41
 
-### The key strokes to page up the lookup table.
-### PAGE_UP_KEYS = Page_Up,KP_Page_Up,minus,comma
+    ### The key strokes to page up the lookup table.
+    ### PAGE_UP_KEYS = Page_Up,KP_Page_Up,minus,comma
 
-### The key strokes to page down.
-### PAGE_DOWN_KEYS = Page_Down,KP_Page_Down,equal,period
+    ### The key strokes to page down.
+    ### PAGE_DOWN_KEYS = Page_Down,KP_Page_Down,equal,period
 
-### The key strokes to select candidiate phrases.
-### Usually "1,2,3,4,5,6,7,8,9" but if this conflicts with
-### characters one wants to use for input one can also
-### use something like “F1,F2,F3,F4,F5,F6,F7,F8,F9”
-SELECT_KEYS = 1,2,3,4,5,6,7,8,9
+    ### The key strokes to select candidiate phrases.
+    ### Usually "1,2,3,4,5,6,7,8,9" but if this conflicts with
+    ### characters one wants to use for input one can also
+    ### use something like “F1,F2,F3,F4,F5,F6,F7,F8,F9”
+    SELECT_KEYS = 1,2,3,4,5,6,7,8,9
 
-### The default orientation of the candidate list
-### TRUE means the candidate list is vertical, FALSE means it is horizontal
-ORIENTATION=TRUE
+    ### The default orientation of the candidate list
+    ### TRUE means the candidate list is vertical, FALSE means it is horizontal
+    ORIENTATION=TRUE
 
-END_DEFINITION
+    END_DEFINITION
 
-### Begin Table data.
-### Format of every line whose formated in "input_keys\\tphrase\\tfreq\\n" is an
-### entry.
-### From left to right, the 1st column are the input key combination that you
-### entered via keyboard; the 2nd column are presented character or phrase of
-### the key combination you want; the 3rd column are frequency of the character
-### or phrase.
-BEGIN_TABLE
-"""
+    ### Begin Table data.
+    ### Format of every line whose formated in "input_keys\\tphrase\\tfreq\\n" is an
+    ### entry.
+    ### From left to right, the 1st column are the input key combination that you
+    ### entered via keyboard; the 2nd column are presented character or phrase of
+    ### the key combination you want; the 3rd column are frequency of the character
+    ### or phrase.
+    BEGIN_TABLE
+    """).lstrip()
     table += to_ibus_fmt(words)
-    table += """
-[	󱦐	1
-]	󱦑	1
-*	‍	1
-+	󱦖	1
--	󱦕	1
-(	󱦗	1
-)	󱦘	1
-_	󱦙	1
-{	󱦚	1
-}	󱦛	1
-.	󱦜	1
-:	󱦝	1
-ss	　	1
-zz	　	1
-<	「	1
->	」	1
-~~	︁	1
-~	︀	1
-END_TABLE"""
+    table += dedent("""
+    [	󱦐	1
+    ]	󱦑	1
+    *	‍	1
+    +	󱦖	1
+    -	󱦕	1
+    (	󱦗	1
+    )	󱦘	1
+    _	󱦙	1
+    {	󱦚	1
+    }	󱦛	1
+    .	󱦜	1
+    :	󱦝	1
+    ss	　	1
+    zz	　	1
+    <	「	1
+    >	」	1
+    ~~	︁	1
+    ~	︀	1
+    END_TABLE""")
 
     return table
 
@@ -355,58 +356,59 @@ def ahk_script(words, short: bool = False, toggle: bool = False, end_chars: str 
     if toggle:
         # ` ^42` center-aligns the string to 42 characters
         # (len of prev line minus the two spaces and semicolons)
-        header = f"""
-; ---- USE ALT + SPACE TO TOGGLE SCRIPT ---- ;
-; {gen_header: ^42} ;
+        header = dedent(f"""
+        ; ---- USE ALT + SPACE TO TOGGLE SCRIPT ---- ;
+        ; {gen_header: ^42} ;
 
-#Requires AutoHotkey v2.0+
-#SingleInstance force
-#Hotstring O Z
+        #Requires AutoHotkey v2.0+
+        #SingleInstance force
+        #Hotstring O Z
 
-Hotstring("EndChars", "{end_chars}")
+        Hotstring("EndChars", "{end_chars}")
 
-#SuspendExempt
-!Space::Suspend
-#SuspendExempt False
+        #SuspendExempt
+        !Space::Suspend
+        #SuspendExempt False
 
-"""
+        """).lstrip()
     else:    
-        header = f"""
-; {gen_header}
+        header = dedent(f"""
+        ; {gen_header}
 
-#Requires AutoHotkey v2.0+
-#SingleInstance force
-#Hotstring O
+        #Requires AutoHotkey v2.0+
+        #SingleInstance force
+        #Hotstring O
 
-Hotstring("EndChars", "{end_chars}")
+        Hotstring("EndChars", "{end_chars}")
 
-"""
-    return header.lstrip() + to_ahk_fmt(words) + """
-::[::{U+F1990} ; cartouche start
-::]::{U+F1991} ; cartouche end
+        """).lstrip()
 
-::=::{U+200D} ; default zero width joiner
-::+::{U+F1996} ; scaling joiner
-::-::{U+F1995} ; stacking joiner
+    return header.lstrip() + to_ahk_fmt(words) + dedent("""
+    ::[::{U+F1990} ; cartouche start
+    ::]::{U+F1991} ; cartouche end
 
-::(::{U+F1997} ; start left-combining (normal) long glyph
-::)::{U+F1998} ; end left-combining (normal) long glyph
-::_::{U+F1999} ; container extender
+    ::=::{U+200D} ; default zero width joiner
+    ::+::{U+F1996} ; scaling joiner
+    ::-::{U+F1995} ; stacking joiner
 
-::{::{U+F199A} ; start right-combining (reversed) long glyph
-::}::{U+F199B} ; end right-combining (reversed) long glyph
+    ::(::{U+F1997} ; start left-combining (normal) long glyph
+    ::)::{U+F1998} ; end left-combining (normal) long glyph
+    ::_::{U+F1999} ; container extender
 
-::.::{U+F199C} ; sitelen pona full stop
-::`:::{U+F199D} ; sitelen pona colon
+    ::{::{U+F199A} ; start right-combining (reversed) long glyph
+    ::}::{U+F199B} ; end right-combining (reversed) long glyph
 
-::`s`s::{U+3000} ; logograph fullwidth space
-::zz::{U+3000} ; logograph fullwidth space
-::<::{U+300C} ; 「 start quote
-::>::{U+300D} ; 」 end quote
+    ::.::{U+F199C} ; sitelen pona full stop
+    ::`:::{U+F199D} ; sitelen pona colon
 
-::~~::{U+FE01} ; variation selector 2
-::~::{U+FE00} ; variation selector 1
-"""
+    ::`s`s::{U+3000} ; logograph fullwidth space
+    ::zz::{U+3000} ; logograph fullwidth space
+    ::<::{U+300C} ; 「 start quote
+    ::>::{U+300D} ; 」 end quote
+
+    ::~~::{U+FE01} ; variation selector 2
+    ::~::{U+FE00} ; variation selector 1
+    """)
 
 # TODO update input plugin (e.g. what else uses `pi_`??)
 def mac_os_plugin(words, short: bool = False) -> str:
@@ -420,37 +422,38 @@ def mac_os_plugin(words, short: bool = False) -> str:
         result.sort(key=sort_key, reverse=True)
         return "\n".join(result)
 
-    return f"""# {gen_header}
+    return dedent(f"""
+    # {gen_header}
 
-METHOD: TABLE
-ENCODE: Unicode
-PROMPT: sitelen pona (UCSUR)
-DELIMITER: ,
-VERSION: 3.2
-MAXINPUTCODE: 15
-VALIDINPUTKEY: aeioujklmnpstw[_].:/+{{}}
+    METHOD: TABLE
+    ENCODE: Unicode
+    PROMPT: sitelen pona (UCSUR)
+    DELIMITER: ,
+    VERSION: 3.2
+    MAXINPUTCODE: 15
+    VALIDINPUTKEY: aeioujklmnpstw[_].:/+{{}}
 
-BEGINCHARACTER
-[ 󱦐
-] 󱦑
-_ 󱦒
-pi_ 󱦓
-___ 󱦔
-- ‍
-^ 󱦕
-+ 󱦖
-( 󱦗
-) 󱦘
-__ 󱦙
-{{ 󱦚
-}} 󱦛
-. 󱦜
-: 󱦝
-te 「
-to 」
-< 「
-> 」
-""" + to_mac_fmt(words) + "\nENDCHARACTER"
+    BEGINCHARACTER
+    [ 󱦐
+    ] 󱦑
+    _ 󱦒
+    pi_ 󱦓
+    ___ 󱦔
+    - ‍
+    ^ 󱦕
+    + 󱦖
+    ( 󱦗
+    ) 󱦘
+    __ 󱦙
+    {{ 󱦚
+    }} 󱦛
+    . 󱦜
+    : 󱦝
+    te 「
+    to 」
+    < 「
+    > 」
+    """).lstrip() + to_mac_fmt(words) + "\nENDCHARACTER"
 
 
 if __name__ == "__main__":
